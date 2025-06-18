6c25161a0fec6aa92ff61eb5ad934bd8322d5076
jbeezy <lemonlimelemonlimelemonlimel@gmail.com>
2025-06-18 14:05:06 -0400

    Does it work? NO!!! But im closer.

--- src/app_elements.rs
+++ src/app_elements.rs
@@ -6,12 +6,18 @@ use egui::*;
 // Intro panel. Shows "GW ANALYZER" at the beginning with the logo for 1 second.
 pub fn intro(ctx: &egui::Context) -> egui::InnerResponse<()> {
     CentralPanel::default().show(ctx, |ui| {
-        ui.allocate_ui_at_rect(rect_center_screen(ctx, 2.5), |ui| {
-            ui.vertical_centered(|ui| {
-                ui.image(egui::include_image!("assets/globe.png"));
-                ui.heading("bottom");
-            });
+            ui.allocate_ui_at_rect(rect_center_screen(ctx, 2.0), |ui| {
+                ui.vertical_centered(|ui| {
+                    ui.image(egui::include_image!("assets/globe.png"));
+                    ui.heading("GW Analyzer");
+                });
         });
+    })
+}
+
+pub fn dev(ctx: &egui::Context) -> egui::InnerResponse<()> {
+    CentralPanel::default().show(ctx, |ui| {
+        ui.heading("Transition");
     })
 }
 
@@ -30,3 +36,7 @@ pub fn rect_center_screen(ctx: &egui::Context, size_cut: f32) -> Rect {
 
     rect
 }
+
+//pub fn run(ctx: &egui::Context, stage: &mut runtime) -> egui::InnerResponse<()> {
+//    if 
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 // Imports
 use eframe::*;
 use egui_extras::*;
+use std::time::Duration;
+use std::thread;
 
 // Resource files
 mod image_handler;
 mod app_elements;
 
-struct GWeather {}
+struct GWeather {
+    pub status: String
+}
+
+impl GWeather {
+    fn default() -> Self {
+        Self {
+            status: String::from("Intro_reading"),
+        }
+    }
+}
 
 impl eframe::App for GWeather {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
@@ -21,13 +33,19 @@ fn main() -> eframe::Result<(), eframe::Error> {
         ..Default::default()
     };
 
+    // Sets timed intro, that's about it. Figuring things out.
+    let gw = GWeather::default();
+    thread::spawn(move || {
+        println!("GW stage: {}", gw.status.clone());
+    });
+
     // Runs here with options
     run_native(
         "GW ANALYZER",
         options,
         Box::new(|cc|{
             egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::new(GWeather {}))
+            Ok(Box::new(gw))
         })
     )
 }
